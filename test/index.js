40efa3777d169e09e4715e3d19dba67075647690
Seungjae Park <astro.psj@gmail.com>
2019-03-16 10:30:20 +0900

    Update lib and test script

--- test/index.js
+++ test/index.js
@@ -1,17 +1,22 @@
 const assert = require('assert');
 
 const inhaInfo = require('../lib');
+const { College, Department } = inhaInfo;
 
 describe('inhaInfo', () => {
   describe('#getAllColleges()', () => {
-    it('', () => {
-      assert.equal();
+    it('Fetch all colleges information.', async () => {
+      const colleges = await inhaInfo.getAllColleges();
+      assert.ok(Array.isArray(colleges));
+      assert.ok(colleges[0] instanceof College);
     });
   });
 
   describe('#getAllDepartments()', () => {
-    it('', () => {
-      assert.equal();
+    it('Fetch all departments information.', async () => {
+      const depts = await inhaInfo.getAllDepartments();
+      assert.ok(Array.isArray(depts));
+      assert.ok(depts[0] instanceof Department);
     });
   });
 });
